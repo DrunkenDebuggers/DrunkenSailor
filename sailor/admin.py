dcca999b63f5b3a2052e8b040dd6131c0dfaf833
B <buherator@silentsignal.hu>
2014-08-21 20:28:50 +0200

    Associate current user with the created project

--- sailor/admin.py
+++ sailor/admin.py
@@ -3,5 +3,11 @@ from sailor.models import Project,Label
 
 # Register your models here.
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    exclude=['creator']
+    def save_model(self,request,obj,form,change):
+        obj.creator = request.user
+        obj.save()
+
+admin.site.register(Project,ProjectAdmin)
 admin.site.register(Label)
