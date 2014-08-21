99ba0b0627c325cb88c0c17ef4fbc4a9fb341bab
B <buherator@silentsignal.hu>
2014-08-21 22:07:13 +0200

    Added automatic redirect

--- drunkensailor/urls.py
+++ drunkensailor/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sailor/', include('sailor.urls')),
+    url(r'^$',lambda r:HttpResponseRedirect('sailor/')),
 )
